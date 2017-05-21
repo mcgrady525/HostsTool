ba07efafa64bae51140e18e6c9e8e451b3f335f8
mcgrady525 <mcgrady_525@163.com>
2017-05-21 12:34:30 +0800

    test

--- DEV/HostsTool/HostsTool/Program.cs
+++ DEV/HostsTool/HostsTool/Program.cs
@@ -8,7 +8,7 @@ namespace HostsTool
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。
+        /// 应用程序的主入口点。   
         /// </summary>
         [STAThread]
         static void Main()
